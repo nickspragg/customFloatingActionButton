a8dd76bac3bea7a2ede4d60eba1d4f2496215494
Nick Spragg <nic@punters.com.au>
2017-10-17 19:53:53 +1100

    added resize support for svg as well as bitmap icons

--- CustomFloatingActionButton/app/src/main/java/com/robertlevonyan/views/customfloatingactionbutton/CustomFloatingActionButtonUtils.java
+++ CustomFloatingActionButton/app/src/main/java/com/robertlevonyan/views/customfloatingactionbutton/CustomFloatingActionButtonUtils.java
@@ -1,8 +1,12 @@
 package com.robertlevonyan.views.customfloatingactionbutton;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 
 /**
  * Created by robert on 3/1/2017.
@@ -22,9 +26,16 @@ class CustomFloatingActionButtonUtils {
     }
 
     static Drawable resize(Context context, Drawable image) {
-        Bitmap b = ((BitmapDrawable)image).getBitmap();
-        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, (int) context.getResources().getDimension(R.dimen.fab_icon_size), (int) context.getResources().getDimension(R.dimen.fab_icon_size), false);
-        return new BitmapDrawable(context.getResources(), bitmapResized);
+        if( image instanceof BitmapDrawable) {
+            Bitmap b = ((BitmapDrawable) image).getBitmap();
+            Bitmap bitmapResized = Bitmap.createScaledBitmap(b, (int) context.getResources().getDimension(R.dimen.fab_icon_size), (int) context.getResources().getDimension(R.dimen.fab_icon_size), false);
+            return new BitmapDrawable(context.getResources(), bitmapResized);
+        }
+        if( image instanceof VectorDrawable) {
+            (image).setBounds(0,0,(int) context.getResources().getDimension(R.dimen.fab_icon_size),(int) context.getResources().getDimension(R.dimen.fab_icon_size));
+            return image;
+        }
+        return image;
     }
 
 }
